7ae5dbe57048b11094baf8386118660a29b689d1
Yohana701 <105068132+Yohana701@users.noreply.github.com>
2024-08-03 13:44:34 -0700

    Create Person.java

---
+++ Person.java
@@ -0,0 +1,16 @@
+public class Person {
+    private String firstName;
+    private String lastName;
+    public Person(String first, String last){
+        firstName = first;
+        lastName = last;
+    }
+    public String getFirstName(){
+        return firstName;
+    }
+    public String getLastName(){
+        return lastName;
+    }
+
+
+}
